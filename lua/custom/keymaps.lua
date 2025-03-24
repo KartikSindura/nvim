e782aab0645550462e0e8798d225bec6e196252f
KartikSindura <kartik.sindura@gmail.com>
2025-03-24 14:33:06 +0530

    added a macro to debug print

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -38,3 +38,78 @@ vim.keymap.set('i', '<C-l>', '<RIGHT>')
 
 -- mini starter
 vim.keymap.set('n', '<leader>ms', ':lua require("mini.starter").open()<CR>', { desc = 'Open [M]ini.[S]tarter' })
+
+-- Table of print statement formats by language
+local print_formats = {
+  javascript = 'console.log("%s: ", %s);',
+  typescript = 'console.log("%s: ", %s);',
+  python = 'print("%s: ", %s)',
+  lua = 'print("%s: ", %s)',
+  go = 'fmt.Println("%s: ", %s)',
+  cpp = 'cout << "%s: " << %s << endl;',
+  java = 'System.out.println("%s: " + %s);',
+  rust = 'println!("%s: {:?}", %s);',
+}
+
+-- Helper function to generate print statement
+local function generate_print_statement(var_name, var_label)
+  local filetype = vim.bo.filetype
+  local format = print_formats[filetype]
+
+  if not format then
+    print('Unsupported language: ' .. filetype)
+    return nil
+  end
+
+  return string.format(format, var_label, var_name)
+end
+
+-- Debug print function for yanked text (visual mode)
+_G.DebugPrintFromYank = function()
+  local var_name = vim.fn.getreg '"' -- Get content from the default register
+
+  if var_name == '' then
+    print 'No variable selected.'
+    return
+  end
+
+  -- For multiline selections, use only the first line for the print label
+  local var_label = var_name:gsub('\n.*', '')
+  -- Limit the label length if it's too long
+  if #var_label > 30 then
+    var_label = var_label:sub(1, 27) .. '...'
+  end
+
+  local print_stmt = generate_print_statement(var_name, var_label)
+  if print_stmt then
+    -- Insert print statement below the current line
+    vim.api.nvim_put({ print_stmt }, 'l', true, true)
+  end
+end
+
+-- Normal mode function
+_G.DebugPrint = function()
+  -- Mark cursor position
+  vim.cmd 'normal! mz'
+
+  -- Get word under cursor
+  local var_name = vim.fn.expand '<cword>'
+
+  if var_name == '' then
+    print 'No variable selected.'
+    return
+  end
+
+  local print_stmt = generate_print_statement(var_name, var_name)
+  if print_stmt then
+    -- Insert print statement below the current line
+    vim.api.nvim_put({ print_stmt }, 'l', true, true)
+
+    -- Restore cursor position
+    vim.cmd 'normal! `z'
+  end
+end
+
+-- Keymaps
+vim.api.nvim_set_keymap('n', '<leader>dp', ':lua _G.DebugPrint()<CR>', { noremap = true, silent = true, desc = '[D]ebug [P]rint (normal mode)' })
+vim.api.nvim_set_keymap('v', '<leader>dp', 'y:lua _G.DebugPrintFromYank()<CR>', { noremap = true, silent = true, desc = '[D]ebug [P]rint (visual mode)' })
