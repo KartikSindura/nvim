d9c4572617b37d2d2f3fae7a9bc51542d38b9dbe
KartikSindura <kartik.sindura@gmail.com>
2025-03-24 15:02:22 +0530

    autocmd for renaming tmux session and windows

--- lua/custom/cmds/autocommands.lua
+++ lua/custom/cmds/autocommands.lua
@@ -11,3 +11,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+if vim.fn.exists '$TMUX' then
+  vim.api.nvim_create_autocmd({ 'BufReadPost', 'FileReadPost', 'BufNewFile' }, {
+    callback = function()
+      local current_file = vim.fn.expand '%:t'
+      vim.fn.system(string.format('tmux rename-window "%s"', current_file))
+    end,
+  })
+
+  vim.api.nvim_create_autocmd({ 'VimLeave' }, {
+    callback = function()
+      vim.fn.system(string.format 'tmux rename-window fish')
+    end,
+  })
+end
